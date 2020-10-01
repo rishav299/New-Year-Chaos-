1e903c5f1bb8589317a0a7a26fbfdd7b1bcebebc
rishav299 <41570190+rishav299@users.noreply.github.com>
2020-10-01 12:17:40 +0530

    Add files via upload

---
+++ NewYearChaos.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumBribes function below.
+def minimumBribes(q):
+    count = 0
+    for i in range(len(q)):
+        if q[i]-(i+1) > 2:
+            print("Too chaotic")
+            return
+        for j in range(max(0,q[i]-2),i):
+            if q[j]>q[i]:
+                count+=1
+    print(count)
+if __name__ == '__main__':
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+
+        q = list(map(int, input().rstrip().split()))
+
+        minimumBribes(q)
